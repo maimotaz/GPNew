2ed69913369e65db0b27e39b9f69c3940b5d1dd9
AlyahKhaled <434203806@student.ksu.edu.sa>
2017-04-30 13:00:28 +0300

    with login

--- app/src/main/java/com/example/dalal/gpnew/login.java
+++ app/src/main/java/com/example/dalal/gpnew/login.java
@@ -1,81 +1,221 @@
 package com.example.dalal.gpnew;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
 
-    public EditText usernameEditText;
-    public EditText passworedEditText;
-    public Button   loginbutton ;
-    public TextView registerTextViw;
-    public TextView changePassTextViw;
-    private String[] arraySpinner;
-    Spinner s;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        this.arraySpinner = new String[] {
-                "Admin", "User", "Restaurant agent"
-        };
-         s = (Spinner) findViewById(R.id.spinner6);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        s.setAdapter(adapter);
+import java.util.HashMap;
 
 
-        loginbutton       = (Button) findViewById(R.id.button2);
-        usernameEditText  = (EditText) findViewById(R.id.editText5);
-        passworedEditText = (EditText) findViewById(R.id.editText9);
-        registerTextViw   = (TextView) findViewById(R.id.textView2);
-        changePassTextViw = (TextView) findViewById(R.id.textView);
 
-        registerTextViw.setOnClickListener (new View.OnClickListener(){
+    public class login extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this,register.class);
-                startActivity(intent);
+        final String TAG = this.getClass().getName();
+
+
+        Button btnLogin;
+        EditText etUsername, etPassword;
+        CheckBox cbRemember;
+        SharedPreferences pref;
+        SharedPreferences.Editor editor;
+        RadioGroup rg;
+        RadioButton rb;
+        String type;
+
+        boolean checkFlag;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+
+
+            rg = (RadioGroup) findViewById(R.id.radioGroup);
+            etUsername = (EditText) findViewById(R.id.etUsername);
+            etPassword = (EditText) findViewById(R.id.etPassword);
+            btnLogin = (Button) findViewById(R.id.btnLogin);
+            cbRemember =(CheckBox)findViewById(R.id.checkBox);
+
+            cbRemember.setOnCheckedChangeListener(this);
+            checkFlag = cbRemember.isChecked();
+            btnLogin.setOnClickListener(this);
+            pref = getSharedPreferences("login.conf", Context.MODE_PRIVATE);
+            editor = pref.edit();
+            editor.clear();
+            editor.commit();
+
+            String username = pref.getString("UserName", "");
+            String password = pref.getString("PassWord", "");
+            Log.d(TAG, pref.getString("PassWord", ""));
+
+            HashMap postData = new HashMap();
+
+            postData.put("txtUsername", username);
+            postData.put("txtPassword", password);
+
+            if(username.contains("Admin")) {
+                if (!(username.equals("") && password.equals(""))) {
+                    PostResponseAsyncTask task1 = new PostResponseAsyncTask(login.this, postData,
+                            new AsyncResponse() {
+                                @Override
+                                public void processFinish(String s) {
+                                    Log.d(TAG, s);
+                                    if (s.contains("success")) {
+                                        Toast.makeText(login.this, "Sucessfully Login", Toast.LENGTH_LONG).show();
+                                        Intent in = new Intent(login.this, admin.class);
+                                        startActivity(in);
+                                    } else {
+                                        Toast.makeText(login.this, " Wrong password or username Try Again", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+
+                    task1.execute("http://zwarh.net/zwarhapp/Alyah/loginAdmin.php");
+
+                }
+
             }
+            else{
+                if (!(username.equals("") && password.equals(""))) {
+                    PostResponseAsyncTask task1 = new PostResponseAsyncTask(login.this, postData,
+                            new AsyncResponse() {
+                                @Override
+                                public void processFinish(String s) {
+                                    Log.d(TAG, s);
+                                    if (s.contains("success")) {
+                                        Toast.makeText(login.this, "Sucessfully Login", Toast.LENGTH_LONG).show();
+                                        Intent in = new Intent(login.this, homeuser.class);
+                                        startActivity(in);
+                                    } else {
+                                        Toast.makeText(login.this, " Wrong password or username Try Again", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
 
-        });
 
+                    task1.execute("http://zwarh.net/zwarhapp/Alyah/login.php");
+
+                }
+            }
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+
+
+            int radId = rg.getCheckedRadioButtonId();
+            rb=(RadioButton) findViewById(radId);
+            type= (String) rb.getText();
+
+            if(type.contains("User")){
+                HashMap postData = new HashMap();
+
+                postData.put("txtUsername", etUsername.getText().toString());
+                postData.put("txtPassword", etPassword.getText().toString());
+
+
+                PostResponseAsyncTask task1 = new PostResponseAsyncTask(login.this, postData,
+                        new AsyncResponse() {
+                            @Override
+                            public void processFinish(String s) {
+                                Log.d(TAG, s);
+                                if(s.contains("success")){
+
+                                    if(checkFlag) {
+                                        editor.putString("UserName", etUsername.getText().toString());
+                                        editor.putString("PassWord", etPassword.getText().toString());
+                                        editor.apply();
+
+                                        Log.d(TAG, pref.getString("PassWord", ""));
+                                    }
+
+                                    Toast.makeText(login.this, "Sucessfully Login", Toast.LENGTH_LONG).show();
+                                    Intent in = new Intent(login.this, homeuser.class);
+                                    startActivity(in);
+                                }
+                                else{
+                                    Toast.makeText(login.this, " Wrong password or username Try Again", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+
+                task1.execute("http://zwarh.net/zwarhapp/Alyah/login.php");
+
+            }
+            else{
+                HashMap postData = new HashMap();
+
+                postData.put("txtUsername", etUsername.getText().toString());
+                postData.put("txtPassword", etPassword.getText().toString());
+
+
+                PostResponseAsyncTask task1 = new PostResponseAsyncTask(login.this, postData,
+                        new AsyncResponse() {
+                            @Override
+                            public void processFinish(String s) {
+                                Log.d(TAG, s);
+                                if(s.contains("success")){
+
+                                    if(checkFlag) {
+                                        editor.putString("UserName", etUsername.getText().toString());
+                                        editor.putString("PassWord", etPassword.getText().toString());
+                                        editor.apply();
+
+                                        Log.d(TAG, pref.getString("PassWord", ""));
+                                    }
+
+                                    Toast.makeText(login.this, "Sucessfully Login", Toast.LENGTH_LONG).show();
+                                    Intent in = new Intent(login.this, admin.class);
+                                    startActivity(in);
+                                }
+                                else{
+                                    Toast.makeText(login.this, " Wrong password or username Try Again", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+
+                task1.execute("http://zwarh.net/zwarhapp/Alyah/loginAdmin.php");
+
+
+            }
+        }
+
+
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            checkFlag = isChecked;
+            Log.d(TAG, "checkFlag: " + checkFlag);
+        }
+
+
+        public void forgetPass(View view){
+            Intent in = new Intent(login.this, forgetpasswored.class);
+            startActivity(in);
+        }
+
+        public void newUser(View view){
+            //Intent in = new Intent(LoginActivity.this, admin.class);
+            // startActivity(in);
+        }
 
     }
-    public void forgetPass(View view) {
-
-        Intent intent = new Intent(login.this,forgetpasswored.class);
-        startActivity(intent);
-
-    }
-
-    public  void  login (View view)
-    {
-         if(s.getSelectedItem().toString().equals("Admin")){
-        Intent intent = new Intent(login.this,admin.class);
-            startActivity(intent);}
-
-        if(s.getSelectedItem().toString().equals("User")){
-            Intent intent2 = new Intent(login.this,homeuser.class);
-        startActivity(intent2);}
-
-        if(s.getSelectedItem().toString().equals("Restaurant agent")){
-         Intent intent3 = new Intent(login.this,homeagent.class);
-            startActivity(intent3);}
-    }
-
-
-
-
-}
